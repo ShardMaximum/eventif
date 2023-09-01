a926d688d1617ec1ee9eb4d684e8ca373a973a7f
Vitor Rocha <61834427+ShardMaximum@users.noreply.github.com>
2023-09-01 12:25:12 +0000

    extends e mudanças nos testes

---
+++ subscriptions/tests/test_mail_subscribe.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from django.core import mail
+
+class MailTest(TestCase):
+    def setUp(self):
+        data = dict(name="Vitor Rocha", 
+                    cpf="12345678901", 
+                    email="vitor.rocha@aluno.riogrande.ifrs.edu.br", 
+                    phone="53-91234-5678")
+        self.response = self.client.post('/inscricao/', data)
+        self.email = mail.outbox[0]
+
+    def test_subscription_email_subject(self):
+        expect = "Confirmação de inscrição"
+        self.assertEqual(expect, self.email.subject)
+
+    def test_subscription_email_sender(self):
+        expect = "contato@eventif.com.br"
+        self.assertEqual(expect, self.email.from_email)
+
+    def test_subscription_email_to(self):
+        expect = ['contato@eventif.com.br', 'vitor.rocha@aluno.riogrande.ifrs.edu.br']
+        self.assertEqual(expect, self.email.to)
+    
+    def test_subscription_email_body(self):
+        contents = ['Vitor Rocha',
+                    '12345678901',
+                    'vitor.rocha@aluno.riogrande.ifrs.edu.br',
+                    '53-91234-5678']
+        for content in contents:
+            with self.subTest():
+                self.assertIn(content, self.email.body)
---
+++ subscriptions/tests/test_view_subscribe.py
@@ -0,0 +1,62 @@
+from django.test import TestCase
+from subscriptions.forms import SubscriptionForm
+from django.core import mail
+
+class SubscribeGet(TestCase):
+    def setUp(self):
+        self.response = self.client.get('/inscricao/')
+
+    def test_get(self):
+        """GET /inscricao/ must return status_code 200"""
+        self.assertEqual(200, response.status_code)
+
+    def test_template(self):
+        """Must use subscriptions/subscription_form.html"""
+        self.assertTemplateUsed(response, 'subscriptions/subscription_form.html')
+
+    def test_html(self):
+        """HTML must contain input tags"""
+        tags = (("<form", 1),
+                ("<input", 6)
+                ('type="text"', 3)
+                ('type="email"', 1)
+                ('type="submit"', 1))
+        for text, count in tags:
+            with self.subTest():
+                self.assertContains(self.response, text, count)
+
+    def test_csrf(self):
+        """"HTML must contain csrf"""
+        self.assertContains(self.response, "csrfmiddlewaretoken")
+
+    def test_has_form(self):
+        """Context must have subscription form"""
+        form = self.response.context["form"]
+        self.assertIsInstance(form, SubscriptionForm)
+    
+class SubscribePostInvalid(TestCase):
+    def setUp(self):
+        self.response = self.client.post('/inscricao/',{})
+
+    def test_post(self):
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        self.assertTemplateUsed(self.response, 'subscriptions/subscription_form.html')
+
+    def test_has_form(self):
+        form = self.response.context['form']
+        self.assertIsInstance(form, SubscriptionForm)
+    
+    def test_form_has_error(self):
+        form = self.response.context['form']
+        self.assertTrue(form.errors)
+
+class SubscribeSuccessMessage(TestCase):
+    def test_message(self):
+        data = dict(name = 'Vitor Rocha',
+                    cpf = '12345678901',
+                    email = 'vitor.rocha@aluno.riogrande.ifrs.edu.br',
+                    phone = '53-91234-5678')
+        response = self.client.post('/inscricao/', data, follow=True)
+        self.assertContains(response, 'Inscrição realizada com sucesso!')
