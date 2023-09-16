444e2dbf53bbef22e9a83d8bbafb03f25323377f
ShardMaximum <vitinho.311204@gmail.com>
2023-09-15 22:53:00 -0300

    consertei uns erros no forms do contact

--- contact/forms.py
+++ contact/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
-class ContactForm(forms.form):
+class ContactForm(forms.Form):
     name = forms.CharField(label='Nome')
     email = forms.EmailField(label='Email')
-    phone = forms.CharField(label='Telefone', blank=True)
+    phone = forms.CharField(label='Telefone', required=False)
     message = forms.CharField(label='Mensagem', widget=forms.Textarea)
