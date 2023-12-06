690552efee9efc48387591c7babab924c170ca95
Vitor Rocha <61834427+ShardMaximum@users.noreply.github.com>
2023-12-06 02:26:12 +0000

    esqueci 1 coisa

--- contact/admin.py
+++ contact/admin.py
@@ -6,3 +6,5 @@ class ContactModelAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_at'
     search_fields = ['name', 'email', 'phone', 'message', 'created_at', 'response', 'response_date']
     list_filter = ['email','created_at','response_date']
+
+admin.site.register(Message, ContactModelAdmin)
